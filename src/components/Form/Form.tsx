c10fe771d3185dfb096c2dd5c5abcf97a7c36a5e
Sb9Ueq833dj6Nrk <kimoung.poly@rupp.edu.kh>
2024-11-02 20:46:54 +0700

    add ReCAPTCHA and update sitemap

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -73,7 +73,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6Lfj9NYfAAAAAP8wPLtzrsSZeACIcGgwuEIRvbSg"
+          sitekey="6LdJMHMqAAAAANkJrI9skHve0wcZSQMmiN6bSqXB"
           onChange={(e) => {
             setIsHuman(true)
           }}
